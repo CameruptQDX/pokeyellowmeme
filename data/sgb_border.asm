7167af22f4af5d4aff046d14d48493982b0ce3d0
CameruptQDX <38517601+CameruptQDX@users.noreply.github.com>
2018-12-11 02:11:18 -0500

    changed to SW silver based border

--- data/sgb_border.asm
+++ data/sgb_border.asm
@@ -1,30 +1,29 @@
 BorderPalettes:
-	INCBIN "gfx/tilemaps/sgbborder.map"
+INCBIN "gfx/tilemaps/sgbborder.map"
 
 	ds $100
-	
+
+
 	RGB 28,28,28 ; PAL_SGB1
-	RGB 0,0,0
-	RGB 31,31,31
-	RGB 31,26,15
+	RGB 31,27,14
+	RGB 19,25,25
+	RGB 16,16,19
 
 	ds $18
 
 	RGB 28,28,28 ; PAL_SGB2
-	RGB 31,26,15
-	RGB 31,12,0
-	RGB 0,0,0
-
+	RGB 31,20,15
+	RGB 31,26,20
+	RGB 16,16,19
 
 	ds $18
 
 	RGB 28,28,28 ; PAL_SGB3
-	RGB 26,28,31
-	RGB 19,20,23
-	RGB 0,0,0
-
+	RGB 29,23,24
+	RGB 19,25,18
+	RGB 16,16,19
 
 	ds $18
 
 SGBBorderGraphics:
-	INCBIN "gfx/pokemon_yellow.t6.2bpp"
+INCBIN "gfx/pokemon_yellow.t6.2bpp"
