557a90a34c268c2d92eb0be502ae8110f565c469
CameruptQDX <38517601+CameruptQDX@users.noreply.github.com>
2018-12-21 01:33:29 -0500

    map border fix

--- data/mapHeaders/silvercaveoutside.asm
+++ data/mapHeaders/silvercaveoutside.asm
@@ -3,5 +3,5 @@ SilverCaveOutside_h:
 	db SILVER_CAVE_OUTSIDE_HEIGHT, SILVER_CAVE_OUTSIDE_WIDTH ; dimensions (y, x)
 	dw SilverCaveOutsideBlocks, SilverCaveOutsideTextPointers, SilverCaveOutsideScript ; blocks, texts, scripts
 	db EAST ; connections
-	EAST_MAP_CONNECTION SILVER_CAVE_OUTSIDE, ROUTE_28, 0, 0, Route28Blocks
+	EAST_MAP_CONNECTION SILVER_CAVE_OUTSIDE, ROUTE_28, 0, -9, Route28Blocks
 	dw SilverCaveOutsideObject ; objects
