89af5ff3d2f518d0f422639d4683373eefa3bb03
CameruptQDX <38517601+CameruptQDX@users.noreply.github.com>
2018-12-29 22:35:52 -0500

    changed trades

--- data/trades.asm
+++ data/trades.asm
@@ -1,8 +1,8 @@
 TradeMons:
 ; givemonster, getmonster, textstring, nickname (11 bytes), 14 bytes total
 	db ARBOK,     DUGTRIO, 0,"THUGTRIO@@@"
-	db CLEFAIRY,  MR_MIME, 0,"JEVIL@@@@@@"
-	db BEEDRILL,  PIKACHU, 2,"EL RATO@@@@" ; unused (will be pewter)
+	db CLEFAIRY,  MR_MIME, 0,"PAGLIACCI@@"
+	db BEEDRILL,FARFETCHD, 2,"Mmmmmmmmmmm" ; unused (will be pewter)
 	db NIDORINO,  MUK,     0,"TRASH MAN@@"
 	db MEWTWO,    MEW,     3,"YOUR SIN@@@" ; unused (will be cerulean cave)
 	db WEEPINBELL,PARASECT,0,"SHIITAKE@@@"
