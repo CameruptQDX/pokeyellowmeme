9b8e31596a24aca76e51583402a9fb0ae98c78dd
CameruptQDX <38517601+CameruptQDX@users.noreply.github.com>
2019-01-01 15:04:15 -0500

    trade name fix

--- data/trades.asm
+++ data/trades.asm
@@ -2,7 +2,7 @@ TradeMons:
 ; givemonster, getmonster, textstring, nickname (11 bytes), 14 bytes total
 	db ARBOK,     DUGTRIO, 0,"THUGTRIO@@@"
 	db CLEFAIRY,  MR_MIME, 0,"PAGLIACCI@@"
-	db BEEDRILL,FARFETCHD, 2,"Mmmmmmmmmmm" ; unused (will be pewter)
+	db BEEDRILL,FARFETCHD, 2,"Mmmmmmmmmm@" ; unused (will be pewter)
 	db NIDORINO,  MUK,     0,"TRASH MAN@@"
 	db MEWTWO,    MEW,     3,"YOUR SIN@@@" ; unused (will be cerulean cave)
 	db RATICATE, PIKACHU,  0,"EL RATO@@@@"
